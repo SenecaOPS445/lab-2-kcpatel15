0cc3dad278b579394a3b32b30f7801beb60914f9
Keval Patel <kcpatel15@myseneca.ca>
2024-05-23 12:52:58 -0400

    Completed Lab2.

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Keval Patel
+#Author ID: kcpatel15@myseneca.ca
+#Date Created: 2024/05/23
+
+timer = 10
+while timer > 0:
+    print (timer)
+    timer = timer -1
+
+print ('blast off!')
